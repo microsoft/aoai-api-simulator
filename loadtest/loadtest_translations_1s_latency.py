8db34daf1464de92d30dcc24a92f28acd1e42588
Stuart Leeks <stuartle@microsoft.com>
2024-11-04 09:48:38 +0000

    wip

--- loadtest/loadtest_translations_1s_latency.py
+++ loadtest/loadtest_translations_1s_latency.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+import requests
 from common.config import api_key, app_insights_connection_string
 from common.latency import set_simulator_translations_latency
 from common.locust_app_insights import (
@@ -34,6 +35,18 @@ def on_locust_init(environment: Environment, **_):
     logging.info("Set translation latencies to 830ms per 1MB (~1s latency for a 1.6MB audio file)")
     set_simulator_translations_latency(environment.host, mean=625, std_dev=0.1)
 
+    # initial request to warm up the deployment (to avoid cold start being included in the latency)
+    with open(".audio/short-white-noise.mp3", "rb") as f:
+        requests.post(
+            environment.host + "/openai/deployments/" + deployment_name + "/audio/translations?api-version=2023-05-15",
+            headers={"api-key": api_key},
+            files={"file": f},
+            data={
+                "response_format": "json",
+            },
+            timeout=10,
+        )
+
     logging.info("on_locust_init - done")
 
 
@@ -45,16 +58,17 @@ class TranslationsUser(HttpUser):
         global got_429_errors, got_other_errors
 
         url = f"openai/deployments/{deployment_name}/audio/translations?api-version=2023-05-15"
-        response = self.client.post(
-            url,
-            headers={"api-key": api_key},
-            files={"file": open(".audio/short-white-noise.mp3", "rb")},
-            data={
-                "response_format": "json",
-            },
-        )
-        if response.status_code >= 300:
-            if response.status_code == 429:
-                got_429_errors = True
-            else:
-                got_other_errors = True
+        with open(".audio/short-white-noise.mp3", "rb") as f:
+            response = self.client.post(
+                url,
+                headers={"api-key": api_key},
+                files={"file": f},
+                data={
+                    "response_format": "json",
+                },
+            )
+            if response.status_code >= 300:
+                if response.status_code == 429:
+                    got_429_errors = True
+                else:
+                    got_other_errors = True
